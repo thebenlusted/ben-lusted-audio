0dc505bf5e6d0256e4b141499207bdcfef77ea27
benjaminlusted <blustedaudio@gmail.com>
2023-06-05 09:14:33 -0600

    -

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Top from './lib/Top.svelte';
+  import Mid from './lib/Mid.svelte';
+  import Bot from './lib/Bot.svelte';
+  import Footer from './lib/Footer.svelte';
+</script>
+
+<main>
+  <Top />
+  <Mid />
+  <Bot />
+  <Footer />
+</main>
+
+<style>
+  
+</style>
